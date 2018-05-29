760283dd7b1284fccad9b0af487f2731e7e7fd0f
SychovSaveliy <savelii.sychov@gettyimages.com>
2018-05-29 21:07:10 +0300

    L_1

--- lections/lection_1_Intro/materials/main.js
+++ lections/lection_1_Intro/materials/main.js
@@ -1,3 +1,91 @@
-console.log('Lection 1');
+console.log("Lection 1");
+
+// oper_name; 
+
+/*   
+
+*/
+
+
+// console.log( -(10 + 23) );
+
+// console.log(203); //number
+// console.log('привет, как дела?', "102");
+// console.log("20323");
+
+
+// a = 20; // присвоить значение
+// b = 30;
+// c = '40';
+
+// res = (a * b) - 200;
+// console.log(res);
+// console.log(a, b, 'a * b =', a * b, c);
+
+// a = 40;
+
+// console.log(a);
+
+
+
+// ----------------------------------
+
+
+// a = 10;
+// a = a + 1;
+
+
+// a = 5;
+// b = 'd';
+
+// // приведение типов
+// res = a + b;
+// console.log('a + b =', res); // number->string + string
+
+// res = a - b;
+// console.log('a - b =', res); // number - string->number
+
+// res = a / b;
+// console.log('a / b =', res); // number / string->number
+
+
+
+// ----------------------------------
+
+// x = +prompt('Input x');
+
+// console.log(typeof x);
+// приведение типов
+
+// 1
+// x = parseInt(x);
+
+// 2
+// x = +x;
+
+// res = 10 + x + 30;
+// console.log(res);
+
+// ----------------------------------
+
+// boolean
+
+// console.log(true, false);
+
+b = true;
+
+// ==, !=, <, <=, >, >=     // === !==
+// res = 10 != 5;
+
+// console.log(res);
+
+a = 13;
+b = 13;
+c = 26;
+
+console.log('a is odd ',  a % 2 == 0);
+
+
+
 
 
