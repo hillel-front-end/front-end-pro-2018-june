f062b40b579387c16f82a8594f07feb185819c62
sychovSaveliy <saveliy.sychov@gmail.com>
2018-08-07 19:10:36 +0300

    L_16 fix

--- lections/lection_16_DOM_practice/materials/main.js
+++ lections/lection_16_DOM_practice/materials/main.js
@@ -28,106 +28,103 @@ window.onload = function(){
     // var menuSec = createMenu(document, items);
 
     document
-        .addEventListener('contextmenu', function(e){
-            e.preventDefault();
-            // debugger;
-
-            menuFirst.style.left = e.offsetX + "px";
-            menuFirst.style.top = e.offsetY + "px";
-            showMenu(menuFirst);
-        })
+        .addEventListener('contextmenu', onContext)
 
     document
         .addEventListener('click', function(e){
             hideMenu(menuFirst);
+            document.addEventListener('contextmenu', onContext);
+            document.removeEventListener('contextmenu', preventDefault);
         })
-    
 
 
+    function onContext(e){
+        e.preventDefault();
+        // debugger;
+
+        document.removeEventListener('contextmenu', onContext);
+        document.addEventListener('contextmenu', preventDefault);
+
+        menuFirst.style.left = e.offsetX + "px";
+        menuFirst.style.top = e.offsetY + "px";
+        showMenu(menuFirst);
+    }
+
+    function preventDefault(ev) {
+        ev.preventDefault();
+    }
 
 
+    function showMenu(target) {
+        target.classList.add('show');
+    }
 
+    function hideMenu(target) {
+        target.classList.remove('show');
+    }
 
-
-
-
-
-
-
-
-
-
-
-        function showMenu(target) {
-            target.classList.add('show');
+    function createMenu(target, list){
+        if (!target || !list) {
+            return;
         }
 
-        function hideMenu(target) {
-            target.classList.remove('show');
+        // var menu = document.createElement('DIV');
+        // menu.classList.add('menu');
+
+        // var item;
+        // for(var i = 0; i < list.length; i++){
+        //     item = document.createElement('div');
+        //     item.classList.add('menu__item');
+        //     item.addEventListener('click', list.actions[list[i].action]);
+
+        //     var icon = document.createElement('span');
+        //     icon.innerHTML = '&rarr;';
+        //     item.appendChild(icon);
+
+        //     var title = document.createElement('span');
+        //     title.innerHTML = list[i].title;
+        //     item.appendChild(title);
+            
+            
+            
+        //     menu.appendChild(item);
+        // }
+
+        // -----------------
+
+        menu = document.querySelector('.menu.pattern').cloneNode();
+        menu.classList.remove('pattern');
+
+        for(var i = 0; i < list.length; i++){
+            item = document
+                .querySelector('.menu__item.pattern')
+                .cloneNode(true);
+            item.classList.remove('pattern');
+
+            item
+                .querySelector('.menu__title')
+                .innerHTML = list[i].title;
+
+            item.addEventListener('click', list.actions[list[i].action]);
+
+            menu.appendChild(item);
         }
 
-        function createMenu(target, list){
-            if (!target || !list) {
-                return;
-            }
 
-            // var menu = document.createElement('DIV');
-            // menu.classList.add('menu');
+        target.appendChild(menu);
 
-            // var item;
-            // for(var i = 0; i < list.length; i++){
-            //     item = document.createElement('div');
-            //     item.classList.add('menu__item');
-            //     item.addEventListener('click', list.actions[list[i].action]);
+        return menu;
+    }
 
-            //     var icon = document.createElement('span');
-            //     icon.innerHTML = '&rarr;';
-            //     item.appendChild(icon);
+    function copyAction(){
+        console.log('copy');
+    }
 
-            //     var title = document.createElement('span');
-            //     title.innerHTML = list[i].title;
-            //     item.appendChild(title);
-                
-                
-                
-            //     menu.appendChild(item);
-            // }
+    function exitAction(){
+        console.log('exit');
+    }
 
-            // -----------------
-
-            menu = document.querySelector('.menu.pattern').cloneNode();
-            menu.classList.remove('pattern');
-
-            for(var i = 0; i < list.length; i++){
-                item = document
-                    .querySelector('.menu__item.pattern')
-                    .cloneNode(true);
-                item.classList.remove('pattern');
-
-                item
-                    .querySelector('.menu__title')
-                    .innerHTML = list[i].title;
-
-                item.addEventListener('click', list.actions[list[i].action]);
-
-                menu.appendChild(item);
-            }
-
-
-            target.appendChild(menu);
-
-            return menu;
-        }
-
-        function copyAction(){
-            console.log('copy');
-        }
-    
-        function exitAction(){
-            console.log('exit');
-        }
-    
-        function cutAction(){
-            console.log('cut');
-        }
+    function cutAction(){
+        console.log('cut');
+    }
 }
